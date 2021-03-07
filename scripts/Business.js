168eb948d34a85c51fbc093cd22c59b3febe2039
Sidney Crandall <sdncrandall5@gmail.com>
2021-03-07 12:29:13 -0600

    New York, agents, and manufactors rendered

--- scripts/Business.js
+++ scripts/Business.js
@@ -1,9 +1,21 @@
 export const nameBusiness = (businessObject) => {
     return `
-        <section class="company">
+        <div class="company">
+        <fieldset>
             <h2 class="companyName">${businessObject.companyName}</h2>
-            <div class="companyAddress"> ${businessObject.addressFullStreet}</div>
-            <div class="companyAddress"> ${businessObject.addressCity}, ${businessObject.addressStateCode}, ${businessObject.addressZipCode}</div>
-        </section>
-    `
+            <div class="companyAddress">${businessObject.addressFullStreet}</div>
+            <div class="companyAddress">${businessObject.addressCity}, ${businessObject.addressStateCode},${businessObject.addressZipCode} </div>
+            </fieldset>
+            </div>
+            `
+}
+export const agentNames = (businessObject) => {
+    return `
+        <div class="company">
+        <fieldset>
+            <h2 class="agentName">${businessObject.agentName}</h2>
+            <div class="companyAddress">${businessObject.company}, ${businessObject.phoneNumber} </div>
+            </fieldset>
+            </div>
+            `
 }
